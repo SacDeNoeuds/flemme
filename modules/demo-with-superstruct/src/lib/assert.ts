8114c8f315bdf0170cb3f44e3cf8657e6bf0cdcb
SacDeNoeuds <92589074+SacDeNoeuds@users.noreply.github.com>
2026-01-21 08:24:51 +0100

    docs(superstruct): remove demo

--- modules/demo-with-superstruct/src/lib/assert.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const isNil = (value: any): value is null | undefined => {
-  return value === null || value === undefined
-}
-export const ensureDefined = <T>(value: T, message = 'Expected value to be defined'): NonNullable<T> => {
-  if (isNil(value)) throw new Error(message)
-  return value as NonNullable<T>
-}
--- modules/demo-with-superstruct/src/lib/form.ts
+++
@@ -1,7 +0,0 @@
-import isEqual from 'fast-deep-equal'
-import { makeLib } from 'flemme'
-import get from 'just-safe-get'
-import set from 'just-safe-set'
-import cloneDeep from 'object-deep-copy'
-
-export const makeForm = makeLib({ get, set, isEqual, cloneDeep })
